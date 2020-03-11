c3af6b1277659f1224811509acdaffd0f51846ca
鳕鱼子 <xueyu.d@gmail.com>
2020-03-11 18:32:44 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const {
+    ApolloServer,
+    gql
+} = require("apollo-server");
+const BooksApi = require("./api/book");
+
+const typeDefs = gql`
+    type Book{
+        title:String
+        id:Int
+        cover_url:String
+    }
+    type Query{
+        books(query:String!):[Book]
+    }
+`;
+
+const resolvers = {
+    Query: {
+        books: (_,{query},{dataSources}) => dataSources.booksApi.searchBooks(query)
+    },
+}
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources:()=>({
+        booksApi:new BooksApi()
+    })
+});
+server.listen({port:80,host:"0.0.0.0"}).then(({
+    url
+}) => {
+    console.log(`🚀 Server ready at ${url}`)
+})
