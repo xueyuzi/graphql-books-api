8c8b150d58eb43000b94317581e2c9c556480504
鳕鱼子 <xueyu.d@gmail.com>
2020-03-15 15:41:12 +0800

    add build

---
+++ h5-client/build/precache-manifest.3e1fa01682706f2b938b102a9ffdddeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a855a17877aa9eaf78d29db4d64dd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95bd8807fd00460f2f7c",
+    "url": "/static/css/2.c015eaef.chunk.css"
+  },
+  {
+    "revision": "4b918c114fdd8ba4da43",
+    "url": "/static/css/main.f958a413.chunk.css"
+  },
+  {
+    "revision": "95bd8807fd00460f2f7c",
+    "url": "/static/js/2.10185268.chunk.js"
+  },
+  {
+    "revision": "283e247fcd70152d45e39e6e5a17cbed",
+    "url": "/static/js/2.10185268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b918c114fdd8ba4da43",
+    "url": "/static/js/main.5555f5c9.chunk.js"
+  },
+  {
+    "revision": "182aaf0557d0125bc4bd",
+    "url": "/static/js/runtime-main.023cc468.js"
+  }
+]);
